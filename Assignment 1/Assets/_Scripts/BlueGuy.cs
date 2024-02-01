610143d9953cc305c37876c82378eb5f4c91dc60
JohnsonHe243 <156115685+JohnsonHe243@users.noreply.github.com>
2024-02-01 14:18:48 -0500

    finished most of functions and added obstacle art
    now to test

--- Assignment 1/Assets/_Scripts/BlueGuy.cs
+++ Assignment 1/Assets/_Scripts/BlueGuy.cs
@@ -7,8 +7,9 @@ using UnityEngine;
 
 public class BlueGuy : AgentObject
 {
-    public GameObject agentSprite;
-    public GameObject targetSprite;
+    public GameObject agent;
+    public GameObject target;
+    public GameObject obstacle;
 
     [SerializeField] float movementSpeed;
     [SerializeField] float rotationSpeed;
@@ -19,6 +20,7 @@ public class BlueGuy : AgentObject
     private Rigidbody2D rb;
     private Vector2 positionA;
     private Vector2 positionT;
+    private Vector2 positionO;
 
 
     new void Start() // Note the new.
@@ -34,16 +36,15 @@ public class BlueGuy : AgentObject
         {
             // Instantiate targetSprite
             positionT = new Vector2(6f, 1f);
-            Instantiate(targetSprite, positionT, Quaternion.identity);
+            Instantiate(target, positionT, Quaternion.identity);
 
             // Parent
             GameObject parent = GameObject.Find("Parent");
 
             // Instantiate agentSprite
-            Vector3 parentPosition = new Vector2(-6.5f, -2.5f); ;
+            Vector3 parentPosition = new Vector2(-6.5f, 1f); ;
             parent.transform.position = parentPosition;
-            positionA = parentPosition;
-            GameObject agent = Instantiate(agentSprite, positionA, Quaternion.identity);
+            Instantiate(agent, parentPosition, Quaternion.identity);
 
             // Set the parent for agent
             agent.transform.parent = parent.transform;
@@ -51,26 +52,157 @@ public class BlueGuy : AgentObject
             Seek();
         }
         else if (Input.GetKeyDown(KeyCode.Alpha2))
-        { 
+        {
+            // Instantiate targetSprite
+            positionT = new Vector2(6f, 1f);
+            Instantiate(target, positionT, Quaternion.identity);
 
+            // Parent
+            GameObject parent = GameObject.Find("Parent");
+
+            // Instantiate agentSprite
+            Vector3 parentPosition = new Vector2(5f, 1f); ;
+            parent.transform.position = parentPosition;
+            Instantiate(agent, parentPosition, Quaternion.identity);
+
+            // Set the parent for agent
+            agent.transform.parent = parent.transform;
             // Flee();
             Flee();
         }
         else if(Input.GetKeyDown(KeyCode.Alpha3))
         {
+            // Instantiate targetSprite
+            positionT = new Vector2(6f, 1f);
+            Instantiate(target, positionT, Quaternion.identity);
 
+            // Parent
+            GameObject parent = GameObject.Find("Parent");
+
+            // Instantiate agentSprite
+            Vector3 parentPosition = new Vector2(-6.5f, -2.5f); ;
+            parent.transform.position = parentPosition;
+            Instantiate(agent, parentPosition, Quaternion.identity);
+
+            // Set the parent for agent
+            agent.transform.parent = parent.transform;
+
+            Arrive();
         }
         else if(!Input.GetKeyDown(KeyCode.Alpha4))
         {
+            // Instantiate targetSprite
+            positionT = new Vector2(6f, 1f);
+            Instantiate(target, positionT, Quaternion.identity);
 
-        }
-        else if(!Input.GetKeyDown(KeyCode.Alpha5))
-        {
+            // Instantiate obstacle
+            positionO = new Vector2(0f, 1f);
+            Instantiate(obstacle, positionO, Quaternion.identity);
+
+            // Parent
+            GameObject parent = GameObject.Find("Parent");
+
+            // Instantiate agentSprite
+            Vector3 parentPosition = new Vector2(-6.5f, -2.5f); ;
+            parent.transform.position = parentPosition;
+            Instantiate(agent, parentPosition, Quaternion.identity);
+
+            // Set the parent for agent
+            agent.transform.parent = parent.transform;
+            
             Seek();
             AvoidObstacles();
         }
+        else if(!Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            Destroy(obstacle);
+            Destroy(agent);
+            Destroy(target);
+        }
     }
 
+    // Cast whiskers to detect obstacles.
+    private bool CastWhisker(float angle)
+    {
+
+        Color rayColor = Color.red;
+        bool hitResult = false;
+
+        
+
+        // Calculate the direction of the whisker
+        Vector2 whiskerDirection = Quaternion.Euler(0, 0, angle) * transform.up;
+
+        // Cast a ray in the whisker direction;
+        RaycastHit2D hit = Physics2D.Raycast(agent.transform.position, whiskerDirection, whiskerLength);
+
+        // Check if the ray hits an obstacle
+        if (hit.collider != null)
+        {
+            Debug.Log("Obstacle Detected!!!");
+
+            rayColor = Color.green;
+            hitResult = true;
+        }
+        Debug.DrawRay(agent.transform.position, whiskerDirection * whiskerLength, rayColor);
+        return hitResult;
+    }
+
+    private void Seek()
+{
+    // Update positions based on GameObjects' actual positions
+    positionA = transform.position;
+    positionT = GameObject.Find("Target(Clone)").transform.position;
+    // Calculate direction to the target.
+    Vector2 direction = (target.transform.position - agent.transform.position).normalized;
+    // Calculate the Velocity
+    Vector2 velocity = direction * movementSpeed;
+    // Set velocity
+    GetComponent<Rigidbody>().velocity = velocity;
+
+    // Calculate the angle to rotate towards the target.
+    //float targetAngle = Mathf.Atan2(directionToTarget.y, directionToTarget.x) * Mathf.Rad2Deg - 90.0f;
+
+    //// Smoothly rotate towards the target.
+    //float angleDifference = Mathf.DeltaAngle(targetAngle, transform.eulerAngles.z);
+    //float rotationStep = rotationSpeed * Time.deltaTime;
+    //float rotationAmount = Mathf.Clamp(angleDifference, -rotationStep, rotationStep);
+    //transform.Rotate(Vector3.forward, rotationAmount);
+
+    //// Move directly towards the target using Rigidbody2D.
+    //rb.velocity = directionToTarget * movementSpeed;
+}
+    private void Flee()
+    {
+        // Calculate direction to the target.
+        Vector2 direction = (agent.transform.position - target.transform.position).normalized;
+        // Calculate the Velocity
+        Vector2 velocity = direction * movementSpeed;
+        // Set velocity
+        GetComponent<Rigidbody>().velocity = velocity;
+    }
+
+    private void Arrive()
+    {
+        // Calculate direction to the target.
+        Vector2 direction = (target.transform.position - agent.transform.position).normalized;
+
+        float distance = direction.magnitude;
+        if (distance < 5f)
+        {
+            // Calculate the Velocity
+            Vector2 velocity = direction.normalized * movementSpeed * (distance / 5f);
+            // Set velocity
+            GetComponent<Rigidbody>().velocity = velocity;
+        }
+        else
+        {
+            // Calculate the Velocity
+            Vector2 velocity = direction * movementSpeed;
+            // Set velocity
+            GetComponent<Rigidbody>().velocity = velocity;
+        }
+    }
     private void AvoidObstacles()
     {
         bool hitleft = CastWhisker(frontWhiskerAngle);
@@ -87,9 +219,7 @@ public class BlueGuy : AgentObject
             // Rotate counterclockwise if the right whisker hit
             RotateCounterClockwise();
         }
-
     }
-
     private void RotateClockwise()
     {
         // Rotate clockwise based on rotationSpeed and a weight.
@@ -100,62 +230,6 @@ public class BlueGuy : AgentObject
         // Rotate counterclockwise based on rotationSpeed and a weight.
         transform.Rotate(Vector3.forward, -rotationSpeed * avoidanceWeight * Time.deltaTime);
     }
-
-
-
-    private bool CastWhisker(float angle)
-    {
-
-        Color rayColor = Color.red;
-        bool hitResult = false;
-        // Cast whiskers to detect obstacles.
-        
-
-
-        // Calculate the direction of the whisker
-        Vector2 whiskerDirection = Quaternion.Euler(0, 0, angle) * transform.up;
-
-        // Cast a ray in the whisker direction;
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, whiskerDirection, whiskerLength);
-
-        // Check if the ray hits an obstacle
-        if (hit.collider != null)
-        {
-            Debug.Log("Obstacle Detected!!!");
-
-            rayColor = Color.green;
-            hitResult = true;
-        }
-        Debug.DrawRay(transform.position, whiskerDirection * whiskerLength, rayColor);
-        return hitResult;
-    }
-
-    private void Seek()
-{
-    // Update positions based on GameObjects' actual positions
-    Vector2 positionA = transform.position;
-    Vector2 positionT = GameObject.Find("Target(Clone)").transform.position;
-
-    // Calculate direction to the target.
-    Vector2 directionToTarget = (positionT - positionA).normalized;
-
-    // Calculate the angle to rotate towards the target.
-    float targetAngle = Mathf.Atan2(directionToTarget.y, directionToTarget.x) * Mathf.Rad2Deg - 90.0f;
-
-    // Smoothly rotate towards the target.
-    float angleDifference = Mathf.DeltaAngle(targetAngle, transform.eulerAngles.z);
-    float rotationStep = rotationSpeed * Time.deltaTime;
-    float rotationAmount = Mathf.Clamp(angleDifference, -rotationStep, rotationStep);
-    transform.Rotate(Vector3.forward, rotationAmount);
-
-    // Move directly towards the target using Rigidbody2D.
-    rb.velocity = directionToTarget * movementSpeed;
-}
-    private void Flee()
-    {
-
-    }
-
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Target")
