ea818611130181e787e4a44bb1e832bc00841fc2
JohnsonHe243 <156115685+JohnsonHe243@users.noreply.github.com>
2024-02-06 13:43:37 -0500

    dragging object according to grid

--- GAME3001_Lab4_Part1_Start/Assets/_MyAssets/_Scripts/GridManager.cs
+++ GAME3001_Lab4_Part1_Start/Assets/_MyAssets/_Scripts/GridManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public enum TileStatus
 {
@@ -26,6 +27,7 @@ public class GridManager : MonoBehaviour
     // Fill in for Lab 4 Part 1.
     [SerializeField] private GameObject tilePrefab;
     [SerializeField] private GameObject 
+    [SerializeField] private Color[] colors;
     private GameObject[,] grid;
     private int rows = 12;
     private int cols = 12;
@@ -52,16 +54,26 @@ public class GridManager : MonoBehaviour
     {
         // Fill in for Lab 4 Part 1.
         BuildGrid();
-        //
+        ConnectGrid();
         //
     }
 
     void Update()
     {
         // Fill in for Lab 4 Part 1.
-        //
-        //
-        //
+        if(Input.GetKeyDown(KeyCode.G)) 
+        {
+            foreach(Transform child in transform)
+            {
+                child.gameObject.SetActive(!child, gameObject.activeSelf);
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.M))
+        {
+            Vector2 gridPosition = GetGridPosition(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+            GameObject mineInst = GameObject.Instantiate(minePrefab, new Vector3(gridPosition.x, gridPosition.y, 0f), Quaternion.identity);
+        }
     }
 
     private void BuildGrid()
@@ -78,14 +90,40 @@ public class GridManager : MonoBehaviour
             { 
                 GameObject tileInst = GameObject.Instantiate(tilePrefab, new Vector3(colPos, rowPos, 0f), Quaternion.identity);
                 tileInst.GetComponent<TileScript>().SetColor(colors[System.Convert.ToInt32((count++ % 2 == 0))]);
+                tileInst.transform.parent = transform;
+
+                grid[row, col] = tileInst;
             }
+            count--;
         }
     }
 
     private void ConnectGrid()
     {
         // Fill in for Lab 4 Part 1.
-        //
+        for(int row = 0; row<rows; row++)
+        {
+            for(int col = 0; col < cols; col++)
+            {
+                TileScript tileScript = grid[row, col].GetComponent<TileScript>();
+                if(row > 0)
+                {
+                    tileScript.SetNeighbourTile((int)NeighbourTile.TOP_TILE, grid[row - 1, col]);
+                }
+                if (col < cols -1)
+                {
+                    tileScript.SetNeighbourTile((int)NeighbourTile.RIGHT_TILE, grid[row, col + 1]);
+                }
+                if (row < row-1)
+                {
+                    tileScript.SetNeighbourTile((int)NeighbourTile.RIGHT_TILE, grid[row + 1, col]);
+                }
+                if (col > 0)
+                {
+                    tileScript.SetNeighbourTile((int)NeighbourTile.RIGHT_TILE, grid[row, col - 1]);
+                }
+            }
+        }
         //
         //
     }
