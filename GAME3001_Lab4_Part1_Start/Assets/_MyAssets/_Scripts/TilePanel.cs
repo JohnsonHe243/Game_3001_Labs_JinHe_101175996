2bc2f9f37de866917a515495e98abbf822b363a5
JohnsonHe243 <156115685+JohnsonHe243@users.noreply.github.com>
2024-02-18 20:08:01 -0500

    tile panel script created

---
+++ GAME3001_Lab4_Part1_Start/Assets/_MyAssets/_Scripts/TilePanel.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TilePanel : MonoBehaviour
+{
+    public TMP_Text tileStatus;
+    public TMP_Text tileCost;
+}
