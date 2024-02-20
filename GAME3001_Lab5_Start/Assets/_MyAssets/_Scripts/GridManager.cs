c530bfd9bc6c125512678bd697006cdfecd85fc0
JohnsonHe243 <156115685+JohnsonHe243@users.noreply.github.com>
2024-02-20 13:38:24 -0500

    click and drag script updated for connect grid after moving mines

--- GAME3001_Lab5_Start/Assets/_MyAssets/_Scripts/GridManager.cs
+++ GAME3001_Lab5_Start/Assets/_MyAssets/_Scripts/GridManager.cs
@@ -147,7 +147,7 @@ public class GridManager : MonoBehaviour
         SetTileCosts(planetIndices);
     }
 
-    private void ConnectGrid()
+    public void ConnectGrid()
     {
         for (int row = 0; row < rows; row++)
         {
